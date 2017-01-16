edf2af9b9d088c0a581acbcbd2f10157c5dbb6e5
Hainee-tyan <cancertyan@mail.ru>
2017-01-16 20:06:58 +1000

    Added actions

--- Calculate.java
+++ Calculate.java
@@ -1,36 +1,77 @@
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class Calculate {
+
+	private static char[] mActions = {'+', '-', '*', '/', '^', '?'};
+	private static double[] mNumbers = new double[2];
+	private static DecimalFormat mDecimalFormat;
+
 	public static void main(String[] args) {
 
 		System.out.println("Calculate...");
 
-		double summ = 0;
-		double product = 1;
-		String output;
+		if (args.length < 2) {
+			System.out.println("Oops... You should input 2 numbers!");
+			return;
+		}
 
 		try {
-
-			for (String arg : args) {
-				arg = arg.replace(',', '.');
-				summ += Double.parseDouble(arg);
-				product *= Double.parseDouble(arg);
-			}
-
-			if (args.length == 0) {
-				output = "Oops, you didn't enter anything!";
-			}
-			else {
-				DecimalFormat df = new DecimalFormat("#.##");
-
-				output = "Your summ is: " + df.format(summ)
-						+ "\nYour product is: " + df.format(product);
+			for (int i = 0; i < 2; i++) {
+				mNumbers[i] = Double.parseDouble(args[i].replace(",", "."));
 			}
 		}
-		catch (NumberFormatException e) {
-			output = "You entered not number!";
+		catch (Exception e) {
+			System.out.println("Oops... You entered not numbers!");
+			return;
 		}
 
-		System.out.println(output);
+		for (int i = 0; i < 6; i++) {
+			printAction(i);
+		}
+	}
+
+	private static void printAction(int actionID) {
+		double result;
+		switch (actionID) {
+			case 0: {
+				result = mNumbers[0] + mNumbers[1];
+				break;
+			}
+			case 1: {
+				result = mNumbers[0] - mNumbers[1];
+				break;
+			}
+			case 2: {
+				result = mNumbers[0] * mNumbers[1];
+				break;	
+			}
+			case 3: {
+				result = mNumbers[0] / mNumbers[1];
+				break;
+			}
+			case 4: {
+				result = Math.pow(mNumbers[0], mNumbers[1]);
+				break;
+			}
+			default: {
+				actionID = 5;
+				result = Double.NaN;
+			}
+		}
+
+		System.out.format("%s %c %s = %s%n", format(mNumbers[0]), mActions[actionID], format(mNumbers[1]), format(result));
+	}
+
+	private static String format(double number) {
+		if (mDecimalFormat == null) {
+			DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+			dfs.setNaN("NaN");
+			dfs.setInfinity("Infinity");
+
+			mDecimalFormat = new DecimalFormat("#.##", dfs);
+		}
+
+		return mDecimalFormat.format(number);
 	}
 }
