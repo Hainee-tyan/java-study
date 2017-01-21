ed19f4f766a14458f02ef931204f233be2dd10d9
Hainee-tyan <cancertyan@mail.ru>
2017-01-21 12:11:41 +1000

    Improved bubble sorting algorithm

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,25 +5,26 @@ public class BubbleSort {
 		return array;
 	}
 
-	public static int[] sort(int[] array) {
-		int length = array.length;
-		for (int j = 0; j < array.length; j++) {
-			System.out.println("j = " + j);
-			for (int i = 0; i < length - 1; i++) {
-				System.out.println("i = " + i);
+	public static void sort(int[] array) {
+
+		for (int j = array.length - 1; j > 0; j--) {
+			for (int i = 0; i < j; i++) {
 				if (array[i] > array[i + 1]) {
-					System.out.println("Swap");
 					swap(array, i, i + 1);
 				}
-				length--;
 			}
 		}
-		return array;
 	}
 
 	private static void swap(int[] mArray, int index1, int index2) {
 		int firstValue = mArray[index1];
 		mArray[index1] = mArray[index2];
 		mArray[index2] = firstValue;
+	}
+
+	private static void print(int[] array) {
+		for (int i : array)
+			System.out.print(i + " ");
+		System.out.println();
 	}
 }
--- Sorting.java
+++ Sorting.java
@@ -1,5 +1,17 @@
 public class Sorting {
 	public static void main(String[] args) {
-		for (String arg : args)
+		int[] array = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			array[i] = Integer.parseInt(args[i]);
+		}
+		System.out.println("Array was parsed!");
+		for (int i : array)
+			System.out.print(i + " ");
+		System.out.println();
+
+		BubbleSort.sort(array);
+
+		for (int i : array)
+			System.out.print(i + " ");
 	}
 }
